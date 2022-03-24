6b1a36d034de875b8aba0bf6bdf0c5e074341b42
lazyshout <lazyshout@gamesoultech.com>
2022-03-24 19:16:31 +0800

    Get sql statements

--- killLog.js
+++ killLog.js
@@ -1,6 +1,26 @@
 const readline = require('readline');
-// const { knexClient } = require('./knexClient');
+const { knexClient } = require('./knexClient');
 
+async function getProcessHistory (processId) {
+    const result = await knexClient.select( 'ps.id', 'ps.user', 'ps.host', 'th.thread_id', 'esh.sql_text')
+                                    .from('information_schema.processlist AS ps')
+                                    .leftJoin('performance_schema.threads AS th', 'th.processlist_id', 'ps.id')
+                                    .leftJoin('performance_schema.events_statements_history AS esh', 'esh.thread_id', 'th.thread_id')
+                                    .where('ps.id', processId)
+                                    .orderBy('esh.EVENT_ID');
+    return result.reduce((summary, row) => {
+        if (!summary.processId) {
+            summary = {
+                processId: row.id,
+                user: row.user,
+                host: row.host,
+                sqlHistory: [],
+            }
+        }
+        summary.sqlHistory.push(row.sql_text);
+        return summary;
+    }, {});
+}
 
 const reader = readline.createInterface({
     input: process.stdin,
@@ -9,7 +29,7 @@ const reader = readline.createInterface({
 
 const regex = /KILL ([0-9]+) /;
 
-reader.on('line', (input) => {
+reader.on('line', async (input) => {
     try {
         const processId = Number.parseInt(input.match(regex)[1]);
         if (processId === NaN || processId === undefined)
@@ -18,6 +38,8 @@ reader.on('line', (input) => {
             
         console.log(`   Received: ${input}`);
         console.log(`   Parsed id: ${processId}`)
+        const result = await getProcessHistory(processId);
+        console.log(result);
     } catch (error) {
         console.log(error);
     }
