2c710521fb485f9b13063570566c2de27b24ba1a
lazyshout <lazyshout@gamesoultech.com>
2022-03-25 12:14:51 +0800

    Add comment

--- pt_example.sh
+++ pt_example.sh
@@ -1,12 +1,16 @@
 
-# DB separate schema for killing? DBA schema
+# --match-user: we should only kill connections that comes from our application server.
+# Be careful not to kill important connection such as things related to replication or aws rds management.
+
+# --wait-before-kill: give our node js logger the chance to retreive connection history before killing it.
 
 pt-kill --host ${DB_HOST:?err} \
 --user ${DB_USER:?err} --password ${DB_PWD:?err} \
---match-command Sleep --match-user 'Test.*' --idle-time 30 \
+--match-user 'Test.*'
+--match-command Sleep --idle-time 30 \
 --victims all \
 --interval 5 \
---create-log-table --log-dsn 'D=Log,t=KilledProcess' \
---wait-before-kill 20 \
+--log-dsn 'D=Log,t=KilledProcess' \
+--wait-before-kill 10 \
 --rds  \
---print | node killLog.js
+--print --kill | node killLog.js
